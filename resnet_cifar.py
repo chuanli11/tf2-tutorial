7c8f7e5963fc02a06b6be499454b01cbef212049
chuanli11 <cl.chuanli@gmail.com>
2019-05-28 14:03:50 -0700

    learning rate in summary

--- resnet_cifar.py
+++ resnet_cifar.py
@@ -10,15 +10,15 @@ import resnet
 HEIGHT = 32
 WIDTH = 32
 NUM_CHANNELS = 3
-num_classes = 10
+NUM_CLASSES = 10
+NUM_TRAIN_SAMPLES = 50000
 
-num_gpus = 2
-num_train_samples = 50000
-bs_per_gpu = 128
-num_epochs = 60
+NUM_GPUS = 2
+BS_PER_GPU = 128
+NUM_EPOCHS = 60
 
 BASE_LEARNING_RATE = 0.1
-LR_SCHEDULE = [(0.1, 30), (0.01, 45), (0.001, 60)]
+LR_SCHEDULE = [(0.1, 30), (0.01, 45)]
 
 def preprocess(x, y):
   x = tf.image.per_image_standardization(x)
@@ -34,13 +34,14 @@ def augmentation(x, y):
 
 
 def schedule(epoch):
-  initial_learning_rate = BASE_LEARNING_RATE * bs_per_gpu / 128
+  initial_learning_rate = BASE_LEARNING_RATE * BS_PER_GPU / 128
   learning_rate = initial_learning_rate
   for mult, start_epoch in LR_SCHEDULE:
     if epoch >= start_epoch:
       learning_rate = initial_learning_rate * mult
     else:
       break
+  tf.summary.scalar('learning rate', data=learning_rate, step=epoch)
   return learning_rate
 
 
@@ -50,13 +51,13 @@ train_loader = tf.data.Dataset.from_tensor_slices((x,y))
 test_loader = tf.data.Dataset.from_tensor_slices((x_test, y_test))
 
 tf.random.set_seed(22)
-train_loader = train_loader.map(augmentation).map(preprocess).shuffle(num_train_samples).batch(bs_per_gpu * num_gpus, drop_remainder=True)
-test_loader = test_loader.map(preprocess).batch(bs_per_gpu * num_gpus, drop_remainder=True)
+train_loader = train_loader.map(augmentation).map(preprocess).shuffle(NUM_TRAIN_SAMPLES).batch(BS_PER_GPU * NUM_GPUS, drop_remainder=True)
+test_loader = test_loader.map(preprocess).batch(BS_PER_GPU * NUM_GPUS, drop_remainder=True)
 
 opt = keras.optimizers.SGD(learning_rate=0.1, momentum=0.9)
 
-if num_gpus == 1:
-    model = resnet.resnet56(classes=num_classes)
+if NUM_GPUS == 1:
+    model = resnet.resnet56(classes=NUM_CLASSES)
     model.compile(
               optimizer=opt,
               loss='sparse_categorical_crossentropy',
@@ -64,7 +65,7 @@ if num_gpus == 1:
 else:
     mirrored_strategy = tf.distribute.MirroredStrategy()
     with mirrored_strategy.scope():
-	    model = resnet.resnet56(classes=num_classes)
+	    model = resnet.resnet56(classes=NUM_CLASSES)
 	    model.compile(
                 optimizer=opt,
 	              loss='sparse_categorical_crossentropy',
@@ -81,7 +82,7 @@ tensorboard_callback = TensorBoard(
 lr_schedule_callback = keras.callbacks.LearningRateScheduler(schedule)
 
 model.fit(train_loader,
-          epochs=num_epochs,
+          epochs=NUM_EPOCHS,
           validation_data=test_loader,
           validation_freq=1,
           callbacks=[tensorboard_callback, lr_schedule_callback])
